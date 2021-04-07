77ad6062deb9f8dc311795429841f94cbcc2a237
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-04-07 17:34:13 -0400

    add article schema add bootstrap

---
+++ db/migrate/20210407212345_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.string :content
+      t.integer :likes
+      t.integer :dislikes
+
+      t.timestamps
+    end
+  end
+end
