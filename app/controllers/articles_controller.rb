4d5ebb2744f0ab43360a64dfaacc8b0a35547eea
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-04-08 18:04:34 -0400

    update controller article fix redirect create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,14 +23,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to articles_url, notice: "Article was created successfully." 
+    else
+      render :new
     end
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :content, presence: true, length: { minimum: 20 }
+  validates :likes, presence: true
+  validates :dislikes, presence: true
 end
