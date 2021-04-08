29eccd514900f5683d639c811992ae8fb3fc7576
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-04-08 17:35:53 -0400

    fix ui fix form fix edit fix new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,14 +36,12 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_url, notice: "Article was updated successfully." 
+    else
+      render :edit
     end
   end
 
